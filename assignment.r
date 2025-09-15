9e06944cd50ee02421a60b4d0b713b8a6edf4c45
zlantaylor <164430549+zlantaylor@users.noreply.github.com>
2025-09-15 11:40:56 -0400

    Update assignment.r

--- assignment.r
+++ assignment.r
@@ -1,8 +1,7 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-M8_Assignment_Dataset <- read_excel("C:/Users/Work/Desktop/M8_Assignment_Dataset.xlsx")
-head(M8_Assignment_Dataset)
+M8_Assignment_Dataset 
 ggplot(M8_Assignment_Dataset, aes(x = Year, y = Black)) + geom_point() + 
   + geom_smooth(method = 'lm', se = FALSE, fullrange = TRUE) +
   ggtitle('Projected Trend in Black Police Trust Levels') +
@@ -11,3 +10,4 @@ ggplot(M8_Assignment_Dataset, aes(x = Year, y = Black)) + geom_point() +
   theme_minimal() + 
   scale_x_continuous(limits = plot_years, breaks = seq(2018, 2028, by = 2))
 
+#geom_point(aes(y = race, color = 'race')) + geom_smooth(aes(y = Race, color = 'Race'), method = 'lm', se = FALSE, fullrange = TRUE)
